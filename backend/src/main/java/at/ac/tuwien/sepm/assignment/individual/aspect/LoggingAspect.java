8ee7b611eef489a41ba1d1ba0d78bda800d62c5c
Lukas Mahler <lukas.mah@outlook.at>
2021-10-05 20:45:58 +0200

    [11] Setup logging for the backend according to TS 11.

---
+++ backend/src/main/java/at/ac/tuwien/sepm/assignment/individual/aspect/LoggingAspect.java
@@ -0,0 +1,68 @@
+package at.ac.tuwien.sepm.assignment.individual.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+@Component
+@Aspect
+@Order(value = 2)
+public class LoggingAspect {
+
+    @Around("execution(* at.ac.tuwien.sepm.assignment.individual..*.*(..))")
+    public Object logMethod(ProceedingJoinPoint joinPoint) throws Throwable {
+        final Logger LOGGER = LoggerFactory.getLogger(joinPoint.getTarget().getClass().getName());
+        Object retVal;
+
+        try {
+
+            StringBuilder startMessageStringBuilder = new StringBuilder();
+
+            startMessageStringBuilder.append("Start method ");
+            startMessageStringBuilder.append(joinPoint.getSignature().getName());
+            startMessageStringBuilder.append("(");
+
+            Object[] args = joinPoint.getArgs();
+            for (Object arg : args) {
+                startMessageStringBuilder.append(arg).append(",");
+            }
+            if (args.length > 0) {
+                startMessageStringBuilder.deleteCharAt(startMessageStringBuilder.length() - 1);
+            }
+            startMessageStringBuilder.append(")");
+            LOGGER.trace(startMessageStringBuilder.toString());
+
+            StopWatch stopWatch = new StopWatch();
+            stopWatch.start();
+
+            retVal = joinPoint.proceed();
+
+            stopWatch.stop();
+
+            String endMessage = "Finish method " +
+                    joinPoint.getSignature().getName() +
+                    "(..); execution time: " +
+                    stopWatch.getTotalTimeMillis() +
+                    " ms;";
+
+            LOGGER.trace(endMessage);
+        } catch (Throwable ex) { // ASK: correct way of logging and throwing??
+
+            // Create error message with exception
+            String errorMessage = "Logging of method " +
+                    joinPoint.getSignature().getName() +
+                    " failed. Logging error message.";
+
+            LOGGER.error(errorMessage, ex);
+            throw ex;
+        }
+
+        return retVal;
+
+    }
+}
--- backend/src/main/java/at/ac/tuwien/sepm/assignment/individual/rest/HorseEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepm/assignment/individual/rest/HorseEndpoint.java
@@ -10,8 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.stream.Stream;
 
 @RestController
-@RequestMapping(path = "/horses")
+@RequestMapping(path = HorseEndpoint.BASE_PATH)
 public class HorseEndpoint {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    static final String BASE_PATH = "/horses";
     private final HorseService service;
     private final HorseMapper mapper;
 
@@ -22,6 +24,8 @@ public class HorseEndpoint {
 
     @GetMapping
     public Stream<HorseDto> allHorses() {
+        LOGGER.info("GET {}: allHorses()", BASE_PATH);
+
         return service.allHorses().stream()
                 .map(mapper::entityToDto);
     }
