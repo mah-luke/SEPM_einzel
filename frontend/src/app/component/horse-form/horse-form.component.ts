2becf4395162cbc04bd267f93b244475c57edc80
Lukas Mahler <lukas.mah@outlook.at>
2021-10-15 19:12:16 +0200

    [1] Refactor horse-form (needed for horse creation) to use reactive forms instead of template forms.

--- frontend/src/app/component/horse-form/horse-form.component.ts
+++ frontend/src/app/component/horse-form/horse-form.component.ts
@@ -1,28 +1,103 @@
-import {Component, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
+import {Component, forwardRef, OnDestroy, OnInit} from '@angular/core';
 import {Sex} from '../../enums/sex';
-import {HorseData} from '../../dto/horseData';
-import {NgForm} from '@angular/forms';
+import {
+  ControlValueAccessor,
+  FormBuilder,
+  FormGroup,
+  NG_VALIDATORS,
+  NG_VALUE_ACCESSOR,
+  Validators
+} from '@angular/forms';
+import {Subscription} from 'rxjs';
+import {HorseFormValues} from '../../dto/horseFormValues';
+
 
 @Component({
   selector: 'app-horse-form',
   templateUrl: './horse-form.component.html',
-  styleUrls: ['./horse-form.component.scss']
+  styleUrls: ['./horse-form.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => HorseFormComponent),
+      multi: true
+    },
+    {
+      provide: NG_VALIDATORS,
+      useExisting: forwardRef(() => HorseFormComponent),
+      multi: true
+    }
+  ]
 })
-export class HorseFormComponent implements OnInit {
-  @ViewChild('horseForm') public form: NgForm;
-
-  @Input() model: HorseData;
-  @Output() emitter = new EventEmitter<HorseData>();
+export class HorseFormComponent implements OnInit, ControlValueAccessor, OnDestroy {
+  subscriptions: Subscription[] = [];
   sexes: Sex[] = Object.values(Sex);
+  formGroup: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {
+    this.formGroup = this.formBuilder.group({
+      name: [null, [Validators.required, Validators.nullValidator]],
+      description: [null],
+      dob: [null, Validators.required],
+      sex: [null, Validators.required],
+      food: [null]
+    });
+
+    this.subscriptions.push(
+      this.formGroup.valueChanges.subscribe(value => {
+        this.onChange(value);
+        this.onTouched();
+      })
+    );
+  }
+
+  get value(){
+    return this.formGroup.value;
+  }
+
+  set value(value: HorseFormValues) {
+    this.formGroup.setValue(value);
+  }
+
+  onTouched: any = () => {};
+
+  onChange: any = () => {};
+
+
+  writeValue(value: HorseFormValues): void {
+      if (value) {
+        this.value = value;
+      }
+
+      if (value === null) {
+        this.formGroup.reset();
+      }
+  }
+
+  registerOnChange(fn: any): void {
+      this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+      this.onTouched = fn;
+  }
+
+  validate(_: any) {
+    return this.formGroup.valid ? null : {profile: {valid: false}};
+  }
 
   ngOnInit(): void {
   }
 
   onSubmit() {
-    this.emitter.emit(this.model);
-    this.form.reset();
+    this.formGroup.reset();
   }
 
+  inputOk(formName: string): boolean {
+    const form = this.formGroup.get(formName);
+    return form.valid || form.pristine;
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(s => s.unsubscribe());
+  }
 }
