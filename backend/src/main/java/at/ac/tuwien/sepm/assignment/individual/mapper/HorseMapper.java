a03a67c2d169cf6553464a688816dd0abdfa52e6
Lukas Mahler <lukas.mah@outlook.at>
2021-10-19 17:47:39 +0200

    [17] Remove 2 not needed methods in HorseMapper (entityToShallowEntity / dtoToEntity)

--- backend/src/main/java/at/ac/tuwien/sepm/assignment/individual/mapper/HorseMapper.java
+++ backend/src/main/java/at/ac/tuwien/sepm/assignment/individual/mapper/HorseMapper.java
@@ -41,31 +41,4 @@ public class HorseMapper {
                 horse.getSex()
         );
     }
-
-    public ShallowHorse entityToShallowEntity(Horse horse) {
-        if (horse == null) return null;
-
-        return new ShallowHorse(
-          horse.getId(),
-          horse.getName(),
-          horse.getDescription(),
-          horse.getDob(),
-          horse.getSex(),
-          horse.getFood()
-        );
-    }
-
-    public Horse dtoToEntity(HorseDto horseDto) {
-        if (horseDto == null) return null;
-
-        Horse horse = new Horse();
-        horse.setId(horseDto.id());
-        horse.setName(horseDto.name());
-        horse.setDescription(horseDto.description());
-        horse.setDob(horseDto.dob());
-        horse.setSex(horseDto.sex());
-        horse.setFood(foodMapper.dtoToEntity(horseDto.food()));
-
-        return horse;
-    }
 }
