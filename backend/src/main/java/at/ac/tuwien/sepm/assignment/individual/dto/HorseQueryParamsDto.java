f8b4de87d0a618783334c2d3905931bbe7f41392
Lukas Mahler <lukas.mah@outlook.at>
2021-10-19 17:45:15 +0200

    [7] Specify DateTimeFormat as annotation in HorseQueryParamsDto for value dob to allow for automatic conversion to LocalDate.

--- backend/src/main/java/at/ac/tuwien/sepm/assignment/individual/dto/HorseQueryParamsDto.java
+++ backend/src/main/java/at/ac/tuwien/sepm/assignment/individual/dto/HorseQueryParamsDto.java
@@ -1,8 +1,8 @@
 package at.ac.tuwien.sepm.assignment.individual.dto;
 
 import at.ac.tuwien.sepm.assignment.individual.enums.Sex;
-
+import org.springframework.format.annotation.DateTimeFormat;
 import java.time.LocalDate;
 
-public record HorseQueryParamsDto(String name, String description, LocalDate dob, Sex sex, Long foodId, Long limit) {
+public record HorseQueryParamsDto(String name, String description, @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dob, Sex sex, Long foodId, Long limit) {
 }
