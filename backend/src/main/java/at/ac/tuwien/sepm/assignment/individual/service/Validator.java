a066720d536126d55e800706d0bca11fde42785f
Lukas Mahler <lukas.mah@outlook.at>
2021-10-21 23:26:02 +0200

    [12] Update JavaDoc for Validator (typos, better explanation, thrown exceptions, etc).

--- backend/src/main/java/at/ac/tuwien/sepm/assignment/individual/service/Validator.java
+++ backend/src/main/java/at/ac/tuwien/sepm/assignment/individual/service/Validator.java
@@ -6,14 +6,14 @@ import at.ac.tuwien.sepm.assignment.individual.exception.StateConflictException;
 import at.ac.tuwien.sepm.assignment.individual.exception.ValidationException;
 
 /**
- * A Validator that validates objects of <Type> that may or may not be saved in the system.
+ * A Validator that validates dtos of <Type> that may or may not be saved in the system.
  *
  * @param <Type> the type of the object to validate.
  */
 public interface Validator<Type> {
 
     /**
-     * Validates an Object of <Type> that has not been persisted in the system.
+     * Validates an dto of <Type> that has not been persisted in the system.
      *
      * @throws ServiceException if an unexpected exception is thrown by the layer below.
      * @throws IllegalArgumentException if the syntax of obj is not valid.
