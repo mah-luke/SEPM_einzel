713d2115a4942ac55d5d045803b0ff9b333230cb
Lukas Mahler <lukas.mah@outlook.at>
2021-10-21 19:02:51 +0200

    [12] Update JavaDoc for HorseDao (typos, better explanation, thrown exceptions, etc).

--- backend/src/main/java/at/ac/tuwien/sepm/assignment/individual/persistence/HorseDao.java
+++ backend/src/main/java/at/ac/tuwien/sepm/assignment/individual/persistence/HorseDao.java
@@ -3,24 +3,31 @@ package at.ac.tuwien.sepm.assignment.individual.persistence;
 import at.ac.tuwien.sepm.assignment.individual.dto.HorseDataDto;
 import at.ac.tuwien.sepm.assignment.individual.dto.HorseQueryParamsDto;
 import at.ac.tuwien.sepm.assignment.individual.entity.Horse;
+import at.ac.tuwien.sepm.assignment.individual.exception.NotFoundException;
+import at.ac.tuwien.sepm.assignment.individual.exception.PersistenceException;
 
 import java.util.List;
 
 /**
  * Data Access Object for horses.
- * Implements access functionality to the application's persistent data store regarding horses.
+ * Enables access to the application's persistent data store for horses.
  */
 public interface HorseDao {
     /**
-     * Get all horses stored in the persistent data store.
+     * List horses stored in the persistent data store by search parameter qParams.
+     *
+     * @throws PersistenceException if an unexpected exception in the data store occured.
      *
      * @return a list of all stored horses
-     * @param qParams the query parameters
+     * @param qParams the valid query parameters
      */
     List<Horse> getAll(HorseQueryParamsDto qParams);
 
     /**
      * Get a specific horse in the persistent data store by its id.
+     *
+     * @throws PersistenceException if an unexpected exception in the data store occured.
+     * @throws NotFoundException if the horse with 'id' does not exist.
      *
      * @param id a valid id of a stored horse
      * @return the stored horse corresponding to the id
@@ -30,15 +37,20 @@ public interface HorseDao {
     /**
      * Create a new horse and store it in the persistent data store.
      *
-     * @param dto the horse to create
+     * @throws PersistenceException if an unexpected exception in the data store occured.
+     *
+     * @param dto the valid horse to create
      * @return the id of the created horse
      */
     Horse createHorse(HorseDataDto dto);
 
     /**
-     * Edit an already existing horse and store the modifications in the persistent data store.
+     * Edit an already existing horse and store its modifications in the persistent data store.
      *
-     * @param dto the horse with the new data
+     * @throws PersistenceException if an unexpected exception in the data store occured.
+     * @throws NotFoundException if the horse with 'id' does not exist.
+     *
+     * @param dto the horse with the new and valid data
      * @return the id of the edited horse
      */
     Horse editHorse(long id, HorseDataDto dto);
@@ -46,7 +58,10 @@ public interface HorseDao {
     /**
      * Delete an already existing horse from the persistent data store.
      *
-     * @param id the horses id
+     * @throws PersistenceException if an unexpected exception in the data store occured.
+     * @throws NotFoundException if the horse to delete doesn't exist
+     *
+     * @param id the horses valid id
      */
     Horse deleteHorse(long id);
 }
