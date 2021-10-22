d32564e96fc24af66d55ac20285c0d9c1415391a
Lukas Mahler <lukas.mah@outlook.at>
2021-10-22 12:48:09 +0200

    [11] Add debug logging for Mappings in FoodMapper

--- backend/src/main/java/at/ac/tuwien/sepm/assignment/individual/mapper/FoodMapper.java
+++ backend/src/main/java/at/ac/tuwien/sepm/assignment/individual/mapper/FoodMapper.java
@@ -2,12 +2,20 @@ package at.ac.tuwien.sepm.assignment.individual.mapper;
 
 import at.ac.tuwien.sepm.assignment.individual.dto.FoodDto;
 import at.ac.tuwien.sepm.assignment.individual.entity.Food;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.lang.invoke.MethodHandles;
 
 @Component
 public class FoodMapper {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
     public FoodDto entityToDto(Food food) {
+        LOGGER.debug("Mapping {} to FoodDto", food);
+
         if (food == null) return null;
 
         return new FoodDto(
