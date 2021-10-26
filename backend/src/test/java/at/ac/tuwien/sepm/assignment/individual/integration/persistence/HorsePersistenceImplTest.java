e847f3fda1bf3fd75051e507af566d64a2e774f2
Lukas Mahler <lukas.mah@outlook.at>
2021-10-26 18:21:42 +0200

    [17] remove unneeded imports in HorsePersistenceImplTest

--- backend/src/test/java/at/ac/tuwien/sepm/assignment/individual/integration/persistence/HorsePersistenceImplTest.java
+++ backend/src/test/java/at/ac/tuwien/sepm/assignment/individual/integration/persistence/HorsePersistenceImplTest.java
@@ -2,17 +2,14 @@ package at.ac.tuwien.sepm.assignment.individual.integration.persistence;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 
-//@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @ActiveProfiles("test")
 public class HorsePersistenceImplTest extends HorsePersistenceTestBase{
